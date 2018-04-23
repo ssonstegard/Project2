e18e1f755470c8229b09dae89d6061dbe5002c5f
ssonstegard <36608691+ssonstegard@users.noreply.github.com>
2018-04-23 13:47:42 -0500

    Add files via upload

---
+++ project2/db/db/seeds.sql
@@ -0,0 +1,98 @@
+use adventure_db;
+
+insert into adventure (place, image, route1, route2, route3, route4)
+values ("MainTown", "\public\assets\images\TheSuck2", 2, 3, 4, 35);
+insert into adventure (place, image, route1, route2, route3)
+values ("Mechanic", "\public\assets\images\TheSuck2", 5, 6, 1);
+insert into adventure (place, image, route1, route2, route3)
+values ("Bar", "\public\assets\images\BoozeryInterior", 11, 12, 13);
+insert into adventure (place, image, route1, route2, route3)
+values ("Mayor", "\public\assets\images\MayorKanter", 18, 19, 1);
+insert into adventure (place, image, route1, route2, route3)
+values ("Transport", "\public\assets\images\TheSuck2", 7, 13, 2);
+insert into adventure (place, image, route1)
+values ("MechRob", "\public\assets\images\TheSuck2", 48);
+insert into adventure (place, image, route1)
+values ("PayTransport", "\public\assets\images\TheSuck2", 9);
+insert into adventure (place, image, route1)
+values ("BoozeTransport", "\public\assets\images\TheSuck2", 10);
+insert into adventure (place, image, route1)
+values ("TransportBuy", "\public\assets\images\TheSuck2", 41);
+insert into adventure (place, image, route1)
+values ("TransportBeer", "\public\assets\images\TheSuck2", 41);
+insert into adventure (place, image, route1, route2, route3)
+values ("BarBiker", "\public\assets\images\BoozeryInterior", 14, 15, 16);
+insert into adventure (place, image, route1)
+values ("BarRob", "\public\assets\images\BoozeryInterior", 48);
+insert into adventure (place, image, route1)
+values ("BarPatron", "\public\assets\images\BoozeryInterior", 17);
+insert into adventure (place, image, route1)
+values ("BikerTransport", "\public\assets\images\Socializing Bikers", 1);
+insert into adventure (place, image, route1)
+values ("BikerStory", "\public\assets\images\Socializing Bikers", 1);
+insert into adventure (place, image, route1, route2, route3)
+values ("BikerJob", "\public\assets\images\Socializing Bikers", 26, 27, 1);
+insert into adventure (place, image, route1)
+values ("BarPatronFight", "\public\assets\images\Barfight", 1);
+insert into adventure (place, image, route1)
+values ("EdenTalk", "\public\assets\images\MayorKanter", 4);
+insert into adventure (place, image, route1, route2, route3)
+values ("MayorJob", "\public\assets\images\MayorKanter", 20, 21, 1);
+insert into adventure (place, image, route1)
+values ("MayorJobNo", "\public\assets\images\MayorKanter", 22 );
+insert into adventure (place, image, route1)
+values ("MayorJobOp", "\public\assets\images\MayorKanter", 23);
+insert into adventure (place, image, route1)
+values ("Prison", "\public\assets\images\Prison", 48);
+insert into adventure (place, image, route1)
+values ("JobYes", "\public\assets\images\MayorKanter", 25);
+insert into adventure (place, image, route1)
+values ("JobNo", "\public\assets\images\MayorKanter", 4);
+insert into adventure (place, image, route1)
+values ("Job1", "\public\assets\images\MayorKanter", 29);
+insert into adventure (place, image, route1)
+values ("BikerJobYes", "\public\assets\images\Socializing Bikers", 28);
+insert into adventure (place, image, route1)
+values ("BikerJobNo", "\public\assets\images\Socializing Bikers", 3);
+insert into adventure (place, image, route1)
+values ("BikerJobDone", "\public\assets\images\Socializing Bikers", 3);
+insert into adventure (place, image, route1, route2, route3)
+values ("MayorJob2", "\public\assets\images\MayorKanter", 18, 30, 1);
+insert into adventure (place, image, route1, route2)
+values ("MayorJob2Yes", "\public\assets\images\MayorKanter", 31, 32);
+insert into adventure (place, image, route1)
+values ("MayorJob2No", "\public\assets\images\MayorKanter", 29);
+insert into adventure (place, image, route1, route2, route3, route4)
+values ("MayorJob2", "\public\assets\images\MayorKanter", 32);
+insert into adventure (place, image, route1, route2, route3)
+values ("Mayor3", "\public\assets\images\MayorKanter", 18, 9, 1);
+insert into adventure (place, image, route1, route2, route3)
+values ("Mayor5", "\public\assets\images\MayorKanter", 18, 47, 1);
+insert into adventure (place, image, route1, route2, route3)
+values ("Wasteland", "\public\assets\images\TheSuck2", 36, 37, 1);
+insert into adventure (place, image, route1)
+values ("WLWalk", "\public\assets\images\TheSuck2", 48);
+insert into adventure (place, image, route1, route2)
+values ("Trailer", "\public\assets\images\TrailerOutside", 38, 39);
+insert into adventure (place, image, route1)
+values ("TrailerWeapon", "\public\assets\images\TrailerInterior", 39);
+insert into adventure (place, image, route1, route2, route3)
+values ("Wasteland2", "\public\assets\images\TheSuck2", 42, 37, 1);
+insert into adventure (place, image, route1)
+values ("TrailerWeaponNo", "\public\assets\images\TrailerInterior", 35);
+insert into adventure (place, image, route1, route2, route3, route4)
+values ("MainTown2", "\public\assets\images\TheSuck2", 2, 3, 4, 39);
+insert into adventure (place, image, route1, route2, route3, route4)
+values ("Eden", "\public\assets\images\TheSuck2", 43, 44, 45, 39  );
+insert into adventure (place, image, route1)
+values ("Papers", "\public\assets\images\TheSuck2", 47);
+insert into adventure (place, image, route1)
+values ("NoPapersInquire", "\public\assets\images\TheSuck2", 46);
+insert into adventure (place, image, route1)
+values ("NoPapersAttempt", "\public\assets\images\TheSuck2", 48);
+insert into adventure (place, image, route1)
+values ("EdenInquire", "\public\assets\images\TheSuck2", 42);
+insert into adventure (place, image, route1)
+values ("Win", "\public\assets\images\EdenCity", 47);
+insert into adventure (place, image, route1)
+values ("GameOver", "\public\assets\images\TheSuck2", 48);
