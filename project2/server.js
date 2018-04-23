5b1120493854d9bdc269b43d23c0e532035e8494
Jayson <jayson@er-10-148-161-135.wireless.umn.edu>
2018-04-23 11:41:00 -0500

    edit

--- project2/server.js
+++ project2/server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
@@ -10,12 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-app.get('/locations/:id', function(req, res){
-    res.sendFile(path.join(__dirname + '/test.js'))
-})
 
 app.listen(PORT, function(){
     console.log("on port: " + PORT);
