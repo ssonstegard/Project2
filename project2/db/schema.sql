65e0dfc5fafda6670aedad4955320f877ee4518b
ssonstegard <36608691+ssonstegard@users.noreply.github.com>
2018-04-23 14:27:40 -0500

    Add files via upload

--- project2/db/schema.sql
+++ project2/db/schema.sql
@@ -4,12 +4,12 @@ create database adventure_db;
 
 use adventure_db;
 
-CREATE TABLE ADVENTURE (
+CREATE TABLE adventure (
   id INT NOT NULL AUTO_INCREMENT,
-  name varchar(40),
-  image varchar (255),
+  place varchar(40),
+  image varchar (500),
   route1 int not null,
-  route2 int not null,
+  route2 int,
   route3 int,
   route4 int,
    PRIMARY KEY (id)
