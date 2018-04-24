254b6877519e30135c465f23c08f47b53c8e273e
Jayson <jaysonchen64@gmail.com>
2018-04-24 14:44:42 -0500

    link text left

--- project2/db/seeds.sql
+++ project2/db/seeds.sql
@@ -8,39 +8,39 @@ values ("Bar", "images/BoozeryInterior.png", 11, 12, 13);
 insert into Adventures (place, image, route1, route2, route3)
 values ("Mayor", "images/MayorKanter.jpg", 18, 19, 1);
 insert into Adventures (place, image, route1, route2, route3)
-values ("Transport", "images/Transport.png", 7, 13, 2);
+values ("Transport", "images/Transport.png", 7, 8, 2);
 insert into Adventures (place, image, route1)
-values ("MechRob", "images/Mechanic.png", 48);
+values ("MechRob", "images/GameOver.png", 48);
 insert into Adventures (place, image, route1)
 values ("PayTransport", "images/Transport.png", 9);
 insert into Adventures (place, image, route1)
 values ("BoozeTransport", "images/Transport.png", 10);
 insert into Adventures (place, image, route1)
-values ("TransportBuy", "images/Transport.png", 41);
+values ("TransportBuy", "images/Car.png", 41);
 insert into Adventures (place, image, route1)
-values ("TransportBeer", "images/Transport.png", 41);
+values ("TransportBeer", "images/Car.png", 41);
 insert into Adventures (place, image, route1, route2, route3)
 values ("BarBiker", "images/BoozeryInterior.png", 14, 15, 16);
 insert into Adventures (place, image, route1)
-values ("BarRob", "images/BoozeryInterior.png", 48);
+values ("BarRob", "images/GameOver.png", 48);
 insert into Adventures (place, image, route1)
 values ("BarPatron", "images/BoozeryInterior.png", 17);
 insert into Adventures (place, image, route1)
-values ("BikerTransport", "images/Socializing Bikers.png", 1);
+values ("BikerTransport", "images/Socializing Bikers.png", 3);
 insert into Adventures (place, image, route1)
-values ("BikerStory", "images/Socializing Bikers.png", 1);
-insert into Adventures (place, image, route1, route2, route3)
-values ("BikerJob", "images/Socializing Bikers.png", 26, 27, 1);
+values ("BikerStory", "images/Socializing Bikers.png", 3);
+insert into Adventures (place, image, route1, route2)
+values ("BikerJob", "images/Socializing Bikers.png", 3, 1);
 insert into Adventures (place, image, route1)
-values ("BarPatronFight", "images/Barfight.png", 1);
+values ("BarPatronFight", "images/Barfight.png", 48);
 insert into Adventures (place, image, route1)
 values ("EdenTalk", "images/MayorKanter.jpg", 4);
 insert into Adventures (place, image, route1, route2, route3)
 values ("MayorJob", "images/MayorKanter2.jpg", 20, 21, 1);
 insert into Adventures (place, image, route1)
 values ("MayorJobNo", "images/MayorKanter2.jpg", 22);
-insert into Adventures (place, image, route1)
-values ("MayorJobOp", "images/MayorKanter2.jpg", 23);
+insert into Adventures (place, image, route1, route2)
+values ("MayorJobOp", "images/MayorKanter2.jpg", 23, 1);
 insert into Adventures (place, image, route1)
 values ("Prison", "images/Prison.png", 48);
 insert into Adventures (place, image, route1)
@@ -48,7 +48,7 @@ values ("JobYes", "images/MayorKanter2.jpg", 25);
 insert into Adventures (place, image, route1)
 values ("JobNo", "images/MayorKanter2.jpg", 4);
 insert into Adventures (place, image, route1)
-values ("Job1", "images/MayorKanter2.jpg", 29);
+values ("Job1", "images/Mechanic.png", 29);
 insert into Adventures (place, image, route1)
 values ("BikerJobYes", "images/Socializing Bikers.png", 28);
 insert into Adventures (place, image, route1)
@@ -60,9 +60,9 @@ values ("MayorJob2", "images/MayorKanter.jpg", 18, 30, 1);
 insert into Adventures (place, image, route1, route2)
 values ("MayorJob2Yes", "images/MayorKanter.jpg", 31, 32);
 insert into Adventures (place, image, route1)
-values ("MayorJob2No", "images/MayorKanter.jpg", 29);
+values ("MayorJob2No", "images/Socializing Bikers.png", 41);
 insert into Adventures (place, image, route1)
-values ("MayorJob2", "images/MayorKanter.jpg", 32);
+values ("MayorJob2", "images/MayorKanter.jpg", 31);
 insert into Adventures (place, image, route1, route2, route3)
 values ("Mayor3", "images/MayorKanter.jpg", 18, 9, 1);
 insert into Adventures (place, image, route1, route2, route3)
@@ -70,13 +70,13 @@ values ("Mayor5", "images/MayorKanter.jpg", 18, 47, 1);
 insert into Adventures (place, image, route1, route2, route3)
 values ("Wasteland", "images/Wasteland.png", 36, 37, 1);
 insert into Adventures (place, image, route1)
-values ("WLWalk", "images/Wasteland.png", 48);
+values ("WLWalk", "images/GameOver.png", 48);
 insert into Adventures (place, image, route1, route2)
 values ("Trailer", "images/TrailerOutside.jpg", 38, 39);
 insert into Adventures (place, image, route1)
 values ("TrailerWeapon", "images/TrailerInterior.jpg", 39);
 insert into Adventures (place, image, route1, route2, route3)
-values ("Wasteland2", "images/Wasteland.jpg", 42, 37, 1);
+values ("Wasteland2", "images/Wasteland.png", 42, 37, 1);
 insert into Adventures (place, image, route1)
 values ("TrailerWeaponNo", "images/TrailerInterior.jpg", 35);
 insert into Adventures (place, image, route1, route2, route3, route4)
@@ -86,12 +86,12 @@ values ("Eden", "images/EdenGate.png", 43, 44, 45, 39);
 insert into Adventures (place, image, route1)
 values ("Papers", "images/EdenGate.jpg", 47);
 insert into Adventures (place, image, route1)
-values ("NoPapersInquire", "images/EdenGate.jpg", 46);
+values ("NoPapersInquire", "images/EdenGate.png", 46);
 insert into Adventures (place, image, route1)
-values ("NoPapersAttempt", "images/EdenGate.jpg", 48);
+values ("NoPapersAttempt", "images/GameOver.png", 48);
 insert into Adventures (place, image, route1)
-values ("EdenInquire", "images/EdenGate.jpg", 42);
+values ("EdenInquire", "images/EdenGate.png", 42);
 insert into Adventures (place, image, route1)
 values ("Win", "images/EdenCity.png", 47);
 insert into Adventures (place, image, route1)
-values ("GameOver", "images/GameOver.png", 48);
+values ("GameOver", "images/GameOver.png", 1);
